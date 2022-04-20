204a465deca7b14dc7dce5d0b1589716f19887b3
Al-Vino Adijaya <98920501+Al-Vino@users.noreply.github.com>
2022-04-20 11:04:26 +0700

    Update crack-4.py

--- crack-4.py
+++ crack-4.py
@@ -1,3 +1,3 @@
-print("\x1b[1;91Script Ini Sedang Diperbarui Ngav")
-print("\x1b[1;93Silahkan Tunggu Beberapa Saat Lagi")
+print("\x1b[1;91mScript Ini Sedang Diperbarui Ngav")
+print("\x1b[1;93mSilahkan Tunggu Beberapa Saat Lagi")
 print("\x1b[1;92m😂Happy Ending😂")
